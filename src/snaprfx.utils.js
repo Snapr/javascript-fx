269404996988eb4b2d0066ff3fe7096232351cb3
Jake Crosby <jakecrosby@gmail.com>
2013-07-18 11:40:47 +1200

    add deferred to jslint globals

--- src/snaprfx.utils.js
+++ src/snaprfx.utils.js
@@ -1,4 +1,4 @@
-/*global SnaprFX: false, JpegMeta: false */
+/*global SnaprFX: false, JpegMeta: false, Deferred:false */
 
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
